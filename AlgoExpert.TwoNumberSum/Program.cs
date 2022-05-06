e2b1504111f3061fd8ee3698e4ff22aa8369b3bb
Lucas <lucassimoes_@hotmail.com>
2022-05-06 06:50:33 -0300

    two number sum best way

--- AlgoExpert.TwoNumberSum/Program.cs
+++ AlgoExpert.TwoNumberSum/Program.cs
@@ -7,24 +7,27 @@ namespace AlgoExpert.TwoNumberSum
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-			int[] result = TwoNumberSum(new int[] { 3, 5, -4, 8, 11, 1, -1, 6 }, 10);
+			int[] result = TwoNumberSum(new int[] { 3, 5, -4, 8, 11, 1, -1, 6 }, 13);
 
 			Console.WriteLine(result[0]);
 			Console.WriteLine(result[1]);
 		}
 		public static int[] TwoNumberSum(int[] array, int targetSum)
 		{
-			for (int i = 0; i < array.Length - 1; i++)
-			{
-				int first = array[i];
-                for (int j = i +1; j < array.Length; j++)
-				{
-					int second = array[j];
-					if (first + second == targetSum)
-						return new int[] { first, second };
-				}
+			Array.Sort(array);
+			int leftPosition = 0;
+			int rightPosition = array.Length - 1;
+			while (leftPosition < rightPosition)
+            {
+				int currentSum = array[leftPosition] + array[rightPosition];
+				if (currentSum.Equals(targetSum))
+					return new int[2] { array[leftPosition], array[rightPosition] };
+				else if (currentSum < targetSum)
+					leftPosition++;
+				else
+					rightPosition--;
 
-			}
+            }
 
 			return new int[0];
 		}
