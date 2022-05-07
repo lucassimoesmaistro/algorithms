7e630b59b453ee5c4e562b09796806c7c507fc82
Lucas <lucassimoes_@hotmail.com>
2022-05-07 16:39:22 -0300

    new tests

---
+++ AlgoExpert.RemoveDuplicatesFromLinkedList/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgoExpert.RemoveDuplicatesFromLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            LinkedList input = new LinkedList(1);
+            addMany(input, new List<int> {
+            1, 3, 4, 4, 4, 5, 6, 6
+             });
+            RemoveDuplicatesFromLinkedList(input);
+
+            Console.WriteLine(input.value);
+            
+        }
+
+        public static LinkedList addMany(LinkedList ll, List<int> values)
+        {
+            Program.LinkedList current = ll;
+            while (current.next != null)
+            {
+                current = current.next;
+            }
+            foreach (var value in values)
+            {
+                current.next = new LinkedList(value);
+                current = current.next;
+            }
+            return ll;
+        }
+
+        public static LinkedList RemoveDuplicatesFromLinkedList(LinkedList linkedList)
+        {
+            LinkedList current = linkedList;
+            while (current != null)
+            {
+                var nextDistinct = current.next;
+                while (nextDistinct != null && current.value == nextDistinct.value)
+                    nextDistinct = nextDistinct.next;
+
+                current.next = nextDistinct;
+                current = nextDistinct;
+            } 
+            return linkedList;
+        }
+
+        public class LinkedList
+        {
+            public int value;
+            public LinkedList next;
+
+            public LinkedList(int value)
+            {
+                this.value = value;
+                this.next = null;
+            }
+
+        }
+    }
+}
---
+++ AlgoExpert.ReverseLinkedList/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgoExpert.ReverseLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            LinkedList input = new LinkedList(1);
+            addMany(input, new List<int> {
+            2, 3, 4, 5
+             });
+            ReverseLinkedList(input);
+        }
+        public static LinkedList ReverseLinkedList(LinkedList linkedList)
+        {
+            var current = linkedList;
+            var head = linkedList;
+            var next = current.Next;
+            current.Next = null;
+            while (next != null)
+            {
+                head = next;
+                next = head.Next;
+                head.Next = current;
+                current = head;
+            }
+            return head;
+        }
+
+        public class LinkedList
+        {
+            public int Value;
+            public LinkedList Next = null;
+
+            public LinkedList(int value)
+            {
+                this.Value = value;
+            }
+        }
+
+        public static LinkedList addMany(LinkedList ll, List<int> values)
+        {
+            Program.LinkedList current = ll;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            foreach (var value in values)
+            {
+                current.Next = new LinkedList(value);
+                current = current.Next;
+            }
+            return ll;
+        }
+    }
+}
