6675c930598e50555c93c0318aa7264c68603db0
Lucas <lucassimoes_@hotmail.com>
2022-05-09 19:26:57 -0300

    binary search without recursive algo

---
+++ AlgoExpert.BinarySearch/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AlgoExpert.BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] array = new int[4] { 1, 5, 23, 111 };
+            int target = 5;
+            int result = BinarySearch(array, target);
+            Console.WriteLine(result);
+        }
+
+        public static int BinarySearch(int[] array, int target)
+        {
+            int left = 0;
+            int rigth = array.Length - 1;
+            while (left != rigth)
+            {
+                int middle = (int)((left + rigth) / 2);
+                if (array[middle] == target)
+                {
+                    left = middle;
+                    break;
+                }
+                if (array[middle] > target)
+                    rigth = middle - 1;
+                else
+                    left = middle + 1;
+            }
+
+            return array[left] == target ? left : -1;
+        }
+    }
+}
