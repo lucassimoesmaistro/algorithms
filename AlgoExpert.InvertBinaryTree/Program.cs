e6e0d0dc1057e8bf1958e0b89a881650a53f3b29
Lucas <lucassimoes_@hotmail.com>
2022-05-17 06:58:28 -0300

    inverted binary tree

---
+++ AlgoExpert.InvertBinaryTree/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace AlgoExpert.InvertBinaryTree
+{
+    class Program
+    {
+		public class BinaryTree
+		{
+			public int value;
+			public BinaryTree left = null;
+			public BinaryTree right = null;
+
+			public BinaryTree(int value)
+			{
+				this.value = value;
+			}
+		}
+		static void Main(string[] args)
+		{
+			BinaryTree root = new BinaryTree(1);
+			root.left = new BinaryTree(2);
+			root.right = new BinaryTree(3);
+			root.left.left = new BinaryTree(4);
+			root.left.right = new BinaryTree(5);
+			root.right.right = new BinaryTree(6);
+			root.left.right.left = new BinaryTree(7);
+			root.left.right.right = new BinaryTree(8);
+			root.right.right.left = new BinaryTree(9);
+			root.right.right.right = new BinaryTree(10);
+			bool expected = true;
+			var actual = new Program().Invert(root);
+			Console.WriteLine(actual.value);
+		}
+		public BinaryTree Invert(BinaryTree tree)
+		{
+			if (tree == null)
+				return tree;
+
+			if (tree.left == null && tree.right == null)
+				return tree;
+
+			BinaryTree leftLastNode, rightLastNode;
+
+			leftLastNode = Invert(tree.left);
+			rightLastNode = Invert(tree.right);
+
+			tree.right = leftLastNode;
+			tree.left = rightLastNode;
+
+			return tree;
+		}
+	}
+}
---
+++ AlgoExpert.MinHeapConstruction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgoExpert.MinHeapConstruction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
