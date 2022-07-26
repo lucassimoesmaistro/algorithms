004630d968937adeba5174b3627bc6567311ab99
Lucas <lucassimoes_@hotmail.com>
2022-07-26 06:01:20 -0300

    dictionary

--- AlgoExpert.MaxPathSumInBinaryTree/Program.cs
+++ AlgoExpert.MaxPathSumInBinaryTree/Program.cs
@@ -5,8 +5,53 @@ namespace AlgoExpert.MaxPathSumInBinaryTree
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+		{
+			BinaryTree root = new BinaryTree(1);
+			root.left = new BinaryTree(2);
+			root.right = new BinaryTree(3);
+			root.left.left = new BinaryTree(4);
+			root.left.right = new BinaryTree(5);
+			root.right.right = new BinaryTree(6);
+			root.right.left = new BinaryTree(7);
+
+			//BinaryTree root = new BinaryTree(1);
+			//root.left = new BinaryTree(-5);
+			//root.right = new BinaryTree(3);
+			//root.left.left = new BinaryTree(6);
+
+			var max = MaxPathSum(root);
+			Console.WriteLine(max);
+		}
+		public static int MaxPathSum(BinaryTree tree)
+		{
+			int leftSum = MaxBranchPathSum(tree.left);
+			int rightSum = MaxBranchPathSum(tree.right);
+			int sum = Math.Max(leftSum + tree.value, tree.value);
+			sum = Math.Max(rightSum + sum, sum);
+
+			return sum;
+		}
+		public static int MaxBranchPathSum(BinaryTree tree)
+		{
+			if (tree.left == null && tree.right == null)
+				return tree.value;
+
+			int max = Math.Max(MaxBranchPathSum(tree.left), MaxBranchPathSum(tree.right));
+			max = Math.Max(max + tree.value, tree.value);
+
+			return max;
+		}
+
+		public class BinaryTree
+		{
+			public int value;
+			public BinaryTree left = null;
+			public BinaryTree right = null;
+
+			public BinaryTree(int value)
+			{
+				this.value = value;
+			}
+		}
+	}
 }
---
+++ Hackerrank.DictionariesAndMaps/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+
+namespace Hackerrank.DictionariesAndMaps
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = Console.Read();
+            if (count >= 0 && count <= 100000)
+            {
+                var phoneBook = GetNameAndPhone(count);
+                var queryNames = QueryNames(count, phoneBook);
+                foreach (var queryName in queryNames)
+                {
+                    if (phoneBook[queryName] != null)
+                        Console.WriteLine($"{queryName}={phoneBook[queryName]}");
+                    else
+                        Console.WriteLine("Not found");
+                }
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+
+        private static string[] QueryNames(int count, Hashtable phoneBook)
+        {
+            string[] queryNames = new string[count];
+            for (int i = 0; i < count; i++)
+            {
+                queryNames[i] = Console.ReadLine();
+            }
+            return queryNames;
+        }
+
+        private static Hashtable GetNameAndPhone(int count)
+        {
+            Hashtable ret = new Hashtable();
+            for(int i = 0; i < count; i++)
+            {
+                string namePhone = Console.ReadLine();
+                ret[namePhone.Split(' ')[0]] = namePhone.Split(' ')[1];
+            }
+            return ret;
+        }
+    }
+}
