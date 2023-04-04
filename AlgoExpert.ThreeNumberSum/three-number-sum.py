8d0fa941d26ac180f39ca1c47ebd53a626b87010
Lucas <lucassimoes_@hotmail.com>
2023-04-04 07:23:06 -0300

    algo

--- AlgoExpert.ThreeNumberSum/three-number-sum.py
+++ AlgoExpert.ThreeNumberSum/three-number-sum.py
@@ -1,14 +1,27 @@
 def threeNumberSum(array, targetSum):
+    sorted_array = sorted(array)
+    print(sorted_array)
     result = []
-    cs = 0
     lpos = 1
-    rpos = len(array)-1
-    left = array[lpos]
-    right = array[rpos]
-    for currentItem in array:
-        if targetSum == (currentItem + left + right):
-            result.append([currentItem, left, right])
-            print(result)
-        
-        
+    rpos = len(sorted_array)-1
+    for currentItem in sorted_array:
+        while lpos < rpos:
+            left = sorted_array[lpos]
+            right = sorted_array[rpos]
+            currentSum = currentItem + left + right
+            print("se target igual a soma", targetSum, currentSum, currentItem, left, right)
+            if targetSum == currentSum:
+                result.append([currentItem, left, right])
+                print(result)
+            elif targetSum > currentSum:
+                lpos+=1
+            elif targetSum < currentSum:
+                rpos-=1
+    print(result)    
     pass
+
+arrayValues = [12,3,1,2,-6,5,-8,6]
+targetSumValue = 0
+
+threeNumberSum(arrayValues,targetSumValue)
+
